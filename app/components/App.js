a0c3ea20b3cda4432c4d48c8b3b6bc208d511980
Gabriel Gomes <gabriel_gom@hotmail.com>
2017-08-31 21:46:04 -0300

    Arquivo package-lock.json adicionado

--- app/components/App.js
+++ app/components/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default class App extends React.Component {
+export default class App extends Component {
     constructor() {
         super();
         
