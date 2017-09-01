c8e4ab9f4b73e85393c1116f90310296fd857f0a
Gabriel Gomes <--global>
2017-08-31 22:49:09 -0300

    Adicao de um input Two-Way Data Binding com o texto

--- app/components/App.js
+++ app/components/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         
         this.state = {
             name: 'World'
         };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            name: event.target.value
+        });
     }
 
     render() {
         return (
             <div className="component--App">
-                Hello <span className="colorful-text">{this.state.name}</span>!
+                <div>
+                    <input type="text" placeholder="Your name here" value={this.state.name} onChange={this.handleChange} />
+                </div>
+                <div>
+                    Hello <span className="colorful-text">{this.state.name}</span>!
+                </div>
             </div>
         );
     }
